959771b9ad0aa543cbddf6fb968a53ff1b82051e
reginaldo <reginaldo.rc.castro@gmail.com>
2026-01-05 11:16:35 -0400

    Criando o model do app

--- src/data_ingestion/models.py
+++ src/data_ingestion/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class DataJob(models.Model):
+    STATUS_CHOICES = (
+        ('PENDING', 'Pending'),
+        ('RUNNING', 'Running'),
+        ('DONE', 'Done'),
+        ('ERROR', 'Error'),
+    )
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.foreignKey(User, on_delete=models.CASCADE)
+    fiel = models.FileField(upload_to='uploads/')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f" {self.id} - {self.status}"
