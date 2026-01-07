b42f90f0708c88121bff9061cacef04b2da29d1a
reginaldo <reginaldo.rc.castro@gmail.com>
2026-01-06 21:09:15 -0400

    Adiconando o caches redis

--- src/core/settings.py
+++ src/core/settings.py
@@ -185,4 +185,12 @@ CHANNELS_LAYERS = {
     },
 }
 
-        
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+    }
+}
