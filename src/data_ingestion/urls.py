07ac43383c3528dfed96ee92ebe24afa08e87021
reginaldo <reginaldo.rc.castro@gmail.com>
2026-01-06 21:10:40 -0400

    Adicionando as urls de listar e detalhes

--- src/data_ingestion/urls.py
+++ src/data_ingestion/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import DataingestionView
+from .views import DataingestionView, DataJobListView, DataJobDetailView
 
 urlpatterns = [
     path('data/', DataingestionView.as_view(), name='data-ingestion'),
+    path('data/jobs/', DataJobListView.as_view(), name='data-job-list'),
+    path('data/jobs/<int:pk>/', DataJobDetailView.as_view(), name='data-job-detail'),
 ]
     
