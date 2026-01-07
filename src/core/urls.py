5cebcf5692350c4c09069747fbfe99c784c8a403
reginaldo <reginaldo.rc.castro@gmail.com>
2026-01-06 21:28:25 -0400

    adicionando a url da documentação da api

--- src/core/urls.py
+++ src/core/urls.py
@@ -20,6 +20,7 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView, 
     TokenRefreshView,
 )
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,4 +29,6 @@ urlpatterns = [
     path('api/v1/', include('data_ingestion.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
